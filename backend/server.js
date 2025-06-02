d9c8458fb06c6f0d0c44737e590911f5bcd9895b
AbhisekhNayek <115914826+AbhisekhNayek@users.noreply.github.com>
2025-06-02 11:39:41 +0530

    fix : cors error

--- backend/server.js
+++ backend/server.js
@@ -28,7 +28,8 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
 app.use(cors({
-  origin: 'http://localhost:5173', // Replace with your frontend URL
+  origin: process.env.CLIENT_URL,
+  credentials: true,
 }));
 
 // Routes
